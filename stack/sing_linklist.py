8a531b74ced35236897799885acc0b2d0ef85df7
taycurran <taylorcurranagtc@gmail.com>
2020-07-14 14:00:16 -0500

    Stack Works Against Tests

---
+++ stack/sing_linklist.py
@@ -0,0 +1,133 @@
+# -----------------------------------------------------------------------------
+class Node:
+    """
+    Data:
+    Stores two pieces of data:
+    1. The Value
+    2. The Next Node
+    Methods/Behavior/Operations:
+    1. Get value
+    2. Set value
+    3. Get next
+    4. Set next
+    """
+    def __init__(self, value=None, next_node=None):
+        # the value at this linked list node
+        self.value = value
+        # reference to the next node in the list
+        # The next node will often be a node itself.
+        self.next_node = next_node
+    def get_value(self):
+        return self.value 
+    def get_next(self):
+        return self.next_node
+    def set_next(self, new_next):
+        # new_next argument is passed a Node Instance
+        # set this node's next_node reference to the passed in node
+        self.next_node = new_next
+        
+class LinkedList:
+    """
+    Data:
+    1. A reference to the head Node
+    2. A reference to the tail Node
+    Behavior/Methods:
+    1. Add To Tail
+    2. Prepend (Add a new node and point that Node's next_node at the old Head; update Head pointer)
+    3. Remove Head
+    4. Remove Tail
+    5. Contains?
+    6. Get Maximum?
+    """
+    def __init__(self):
+        # reference to the head of the list
+        self.head = None
+        # reference to the tail of the list
+        self.tail = None
+    
+    def contains(self, value):
+        if not self.head:
+            return False
+        else:
+            cont = []
+            current_node = self.head
+            while current_node.next_node:
+                cont.append(current_node.value)
+                current_node = current_node.next_node
+            cont.append(self.tail.value)
+            if value in cont:
+                return True
+            else:
+                return False
+
+    def add_to_tail(self, value):
+        # wrap the input value in a node
+        new_node = Node(value, None)
+        # check if there is no head (i.e., the list is empty)
+        if not self.head:
+            # if the list is initially empty, set both head and tail to the new node
+            self.head = new_node
+            self.tail = new_node
+        # we have a non-empty list, add the new node to the tail
+        else:
+            # set the current tail's next reference to our new node
+            self.tail.set_next(new_node)
+            # set the list's tail reference to the new node
+            self.tail = new_node
+
+    def remove_head(self):
+        # return None if there is no head (i.e. the list is empty)
+        if not self.head:
+            return None
+        # if head has no next, then we have a single element in our list
+        if not self.head.get_next():
+            # get a reference to the head
+            head = self.head
+            # delete the list's head reference
+            self.head = None
+            # also make sure the tail reference doesn't refer to anything
+            self.tail = None
+            # return the value
+            return head.get_value()
+        # otherwise we have more than one element in our list
+        value = self.head.get_value()
+        # set the head reference to the current head's next node in the list
+        self.head = self.head.get_next()
+        return value
+
+    def remove_tail(self):
+        if not self.head:
+            return None
+        
+        if self.head is self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+
+        current = self.head
+
+        while current.get_next() is not self.tail:
+            current = current.get_next()
+
+        value = self.tail.get_value()
+        self.tail = current
+        self.tail.set_next(None)
+        return value
+
+    def get_max(self):
+        if not self.head:
+            return None
+        # reference to the largest value we've seen so far
+        max_value = self.head.get_value()
+        # reference to our current node as we traverse the list
+        current = self.head.get_next()
+        # check to see if we're still at a valid list node
+        while current:
+            # check to see if the current value is greater than the max_value
+            if current.get_value() > max_value:
+                # if so, update our max_value variable
+                max_value = current.get_value()
+            # update the current node to the next node in the list
+            current = current.get_next()
+        return max_value
--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,4 @@
+# from singly_linked_list/singly_linked_list.py import Node, LinkedList
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -10,23 +11,68 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+from sing_linklist import Node, LinkedList
+
 class Stack:
-     def __init__(self):
-         self.size = 0
-        #self.storage = ?
-         self.items = []
+    def __init__(self):
+        self.size = 0
+        # self.storage = ?
+        self.head = None
+        #self.tail = None
 
-     def isEmpty(self):
-         return self.items == []
+    def __len__(self):
+        if not self.head:
+            self.size = 0
+            return self.size
+        else:
+            cont = []
+            current_node = self.head
+            while current_node.next_node:
+                cont.append(current_node.value)
+                current_node = current_node.next_node
+            cont.append(self.tail.value)  
+            self.size = len(cont)
+            return self.size
 
-     def push(self, item):
-         self.items.append(item)
+    def push(self, value):
+        if not self.head:
+            new_node = Node(value, None)
+            self.head = new_node
+            self.tail = new_node
+        else:
+            orig_head = self.head
+            new_node = Node(value, orig_head)
+            new_head = new_node
+            self.head = new_head
+            
+    def pop(self):
+        if not self.head:
+            self.head = None
+            self.tail = None
+        else:
+            orig_head_value = self.head.value
+            self.head = self.head.next_node
+            return orig_head_value
 
-     def pop(self):
-         return self.items.pop()
 
-     def peek(self):
-         return self.items[len(self.items)-1]
+# class Stack:
+#      def __init__(self):
+#          self.size = 0
+#         #self.storage = ?
+#          self.items = []
 
-     def size(self):
-         return len(self.items)
+#      def isEmpty(self):
+#          return self.items == []
+
+#      def push(self, item):
+#          self.items.append(item)
+
+#      def pop(self):
+#          return self.items.pop()
+
+#      def peek(self):
+#          return self.items[len(self.items)-1]
+
+#      def size(self):
+#          return len(self.items)
